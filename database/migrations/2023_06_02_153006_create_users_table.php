1e507746e69e699a3ae41b3ba2336b42656e6146
lkw <xtkf01@yunhuipost.com>
2023-06-21 18:04:17 +0800

    重新设计user表

--- database/migrations/2023_06_02_153006_create_users_table.php
+++ database/migrations/2023_06_02_153006_create_users_table.php
@@ -18,11 +18,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create(self::TABLE, function (Blueprint $table) {
             $table->integerIncrements('id');
-            $table->string('account', 64)->comment('账号');
-            $table->string('password')->comment('密码');
-            $table->string('register_address')->comment('注册地址');
-            $table->string('last_location')->default('')->comment('用户最后登录位置');
-            $table->unsignedInteger('last_ip')->comment('用户最后登录IP');
+            $table->string('username', 32)->comment('账号');
+            $table->string('password',128)->comment('密码');
+            $table->tinyInteger('sex')->default(2)->comment('性别：0女、1男、2保密');
+            $table->string('register_address',100)->default('')->comment('注册地址');
+            $table->tinyInteger('status')->default(1)->comment('状态：1启用、0禁用');
+            $table->unsignedInteger('login_num')->default(0)->default(0)->comment('用户登录次数');
+            $table->string('last_location',100)->default('')->comment('用户最后登录位置');
+            $table->dateTime('last_login_time')->default(null)->nullable()->comment('用户最后登录时间');
+            $table->string('last_ip',15)->default('')->comment('用户最后登录IP');
             $table->timestamps();
         });
         DB::statement('ALTER TABLE ' . self::TABLE . ' COMMENT "用户表"');
